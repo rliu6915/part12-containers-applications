68c7dde981b702611dce366d2d9a68df72fcaa0b
rliu6915 <rliu6915@connect.hku.hk>
2024-02-12 11:55:07 +0800

    12.21

--- my-app/frontend/src/services/parcels.js
+++ my-app/frontend/src/services/parcels.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
-const baseUrl = '/api/calShippingFees'
+// const baseUrl = '/api/calShippingFees'
+const baseUrl = 'http://localhost:3001/api/calShippingFees'
 
 const create = async (newParcels) => {
   // console.log("newParcels", newParcels)
